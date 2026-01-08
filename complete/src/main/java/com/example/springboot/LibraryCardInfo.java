adf10b3d041e99f68ead07d570338d07450d8e40
Kevin Matthews <44195753+CSMonkee@users.noreply.github.com>
2026-01-08 13:04:00 +0000

    Create LibraryCardInfo class for library card details
    Signed-off-by: Kevin Matthews <44195753+CSMonkee@users.noreply.github.com>

---
+++ complete/src/main/java/com/example/springboot/LibraryCardInfo.java
@@ -0,0 +1,13 @@
+public class LibraryCardInfo {
+    private String firstName;
+    private String lastName;
+    private String cardNumber;
+    private Date validUntil;
+
+    public LibraryCardInfo(String firstName, String lastName, String cardNumber, Date validUntil) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cardNumber = cardNumber;
+        this.validUntil = validUntil;
+    }
+}
